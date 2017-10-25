bbbc62d9a8a1b8b5f987b5bdca45fc517b3c9c59
Patric Campbell <patric@patriccampbell.com>
2017-10-25 18:01:22 -0400

    Create a searchBar component for the video search.

---
+++ reactRedux/ReduxSimpleStarter/src/components/searchBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: ""
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.onInputChange} value={this.state.searchTerm} />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
--- reactRedux/ReduxSimpleStarter/src/index.js
+++ reactRedux/ReduxSimpleStarter/src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import SearchBar from "./components/searchBar";
+
 const API_KEY = "AIzaSyDR3tdSPywLlW5qIVC8rbi2EsH2NaBRFXE";
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector(".container"));
