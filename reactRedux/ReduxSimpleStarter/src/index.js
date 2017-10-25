af989ee9e784b615579c5c492b448398df34f9f9
Patric Campbell <patric@patriccampbell.com>
2017-10-25 17:28:34 -0400

    Add component file structure

---
+++ reactRedux/ReduxSimpleStarter/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const API_KEY = "AIzaSyDR3tdSPywLlW5qIVC8rbi2EsH2NaBRFXE";
+
+const App = () => {
+  return <div>Hi!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector(".container"));
