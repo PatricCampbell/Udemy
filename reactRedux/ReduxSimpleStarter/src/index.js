fc8c921dd877cdfa0c37160a682aa239ea9ce629
Patric Campbell <patric@patriccampbell.com>
2017-10-26 11:18:43 -0400

    Change App to a class component

--- reactRedux/ReduxSimpleStarter/src/index.js
+++ reactRedux/ReduxSimpleStarter/src/index.js
@@ -1,16 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/searchBar";
 
 const API_KEY = "AIzaSyDR3tdSPywLlW5qIVC8rbi2EsH2NaBRFXE";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+YTSearch({ key: API_KEY, term: "surfboards" }, data => {
+  console.log(data);
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
