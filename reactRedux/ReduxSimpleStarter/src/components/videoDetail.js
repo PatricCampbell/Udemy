c0414c3550bd4bd3b94d510a14a79f531bf56b17
Patric Campbell <patric@patriccampbell.com>
2017-10-26 11:57:46 -0400

    Add videoDetail component to display a single video in an iframe

---
+++ reactRedux/ReduxSimpleStarter/src/components/videoDetail.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+
+  const videoTitle = video.snippet.title;
+  const videoDesc = video.snippet.description;
+  const videoId = video.id.videoId;
+  const videoUrl = `https://www.youtube.com/embed/${videoId}`;
+
+  return (
+    <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={videoUrl} />
+      </div>
+      <div className="details">
+        <div>{videoTitle}</div>
+        <div>{videoDesc}</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
--- reactRedux/ReduxSimpleStarter/src/index.js
+++ reactRedux/ReduxSimpleStarter/src/index.js
@@ -4,6 +4,7 @@ import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/searchBar";
 import VideoList from "./components/videoList";
+import VideoDetail from "./components/videoDetail";
 
 const API_KEY = "AIzaSyDR3tdSPywLlW5qIVC8rbi2EsH2NaBRFXE";
 
@@ -22,6 +23,7 @@ class App extends React.Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
