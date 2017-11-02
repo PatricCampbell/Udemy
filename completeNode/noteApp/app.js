52734a44239587a5cfe371060360a9a524a0bdb0
Patric Campbell <patric@patriccampbell.com>
2017-11-02 16:33:47 -0400

    Clean up files of practice parts

--- completeNode/noteApp/app.js
+++ completeNode/noteApp/app.js
@@ -1,24 +1,6 @@
 console.log("Starting app.js");
 
 const fs = require("fs");
-const os = require("os");
+const _ = require("lodash");
 
 const notes = require("./notes");
-
-// const res = notes.addNote();
-// console.log(res);
-
-const add = notes.add(5, 9);
-
-console.log(add);
-// const user = os.userInfo();
-
-// fs.appendFile(
-//   "greetings.txt",
-//   `Hello, ${user.username}! You are ${notes.age}.`,
-//   err => {
-//     if (err) {
-//       console.log(err);
-//     }
-//   }
-// );
