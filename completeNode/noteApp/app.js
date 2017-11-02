2f994bdf5f4112f9fdb2daf64de899b2018085a0
Patric Campbell <patric@patriccampbell.com>
2017-11-02 16:05:29 -0400

    Add notes file to hold functions for dealing with the notes

--- completeNode/noteApp/app.js
+++ completeNode/noteApp/app.js
@@ -1,12 +1,24 @@
-console.log("Starting app");
+console.log("Starting app.js");
 
 const fs = require("fs");
 const os = require("os");
 
-const user = os.userInfo();
+const notes = require("./notes");
 
-fs.appendFile("greetings.txt", `Hello, ${user.username}`, err => {
-  if (err) {
-    console.log(err);
-  }
-});
+// const res = notes.addNote();
+// console.log(res);
+
+const add = notes.add(5, 9);
+
+console.log(add);
+// const user = os.userInfo();
+
+// fs.appendFile(
+//   "greetings.txt",
+//   `Hello, ${user.username}! You are ${notes.age}.`,
+//   err => {
+//     if (err) {
+//       console.log(err);
+//     }
+//   }
+// );
