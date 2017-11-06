764494767dce82c37687251045d976ce39d94d44
Patric Campbell <patric@patriccampbell.com>
2017-11-06 18:12:54 -0500

    Add error handling in the request call

--- completeNode/weatherApp/app.js
+++ completeNode/weatherApp/app.js
@@ -1,17 +1,37 @@
 const request = require("request");
-require("dotenv").config();
+const yargs = require("yargs");
 
+require("dotenv").config();
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch weather for.",
+      string: true
+    }
+  })
+  .help()
+  .alias("help", "h").argv;
+
+const encodedAddress = encodeURIComponent(argv.address);
 
 request(
   {
-    url:
-      "https://maps.googleapis.com/maps/api/geocode/json?address=750%20ocean%20avenue%20brooklyn",
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
   },
   (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    if (error) {
+      console.log("Unable to connect to google servers.");
+    } else if (body.status === "ZERO_RESULTS") {
+      console.log("Unable to find that address ");
+    } else if (body.status === "OK") {
+      console.log(`Address: ${body.results[0].formatted_address}`);
+      console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
   }
 );
