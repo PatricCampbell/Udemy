9054a64cc19e2fa4b1a741219f37b309f14ef479
Patric Campbell <patric@patriccampbell.com>
2017-11-07 11:06:14 -0500

    Add ability to get the temperature and apparent temperature for a given address

--- completeNode/weatherApp/app.js
+++ completeNode/weatherApp/app.js
@@ -19,18 +19,14 @@ geocode.geocodeAddress(argv.address, (error, results) => {
   if (error) {
     console.log(error);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    weather.getWeather(results.lat, results.lng, (error, weatherResults) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(
+          `The temperature is ${weatherResults.temp}, but it feels like ${weatherResults.apparentTemp}`
+        );
+      }
+    });
   }
 });
-
-// weather.getWeather("40.504845", "-79.856194", (error, results) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log(
-//       `The temperature is ${results.temp}, but it feels like ${results.apparentTemp}`
-//     );
-//   }
-// });
-//make getWeather like geocode function and use lat, lng, and callback, callback will take error and results
-// get temperature and actual temperature
