f7627465bb5a79c3811aee2181a3152e29139134
Patric Campbell <patric@patriccampbell.com>
2017-11-08 12:29:45 -0500

    Try out some routing.

--- completeNode/webServer/server.js
+++ completeNode/webServer/server.js
@@ -3,7 +3,17 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+  res.send("<h1>Hello, Express!</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    error: "Error! Error! Error!"
+  });
 });
 
 app.listen(3000);
