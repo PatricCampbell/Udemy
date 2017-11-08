5bcfbffcc0a6ccabc0bccbb17592c41eeba6202c
Patric Campbell <patric@patriccampbell.com>
2017-11-08 12:50:54 -0500

    Add about and home pages with simple data

--- completeNode/webServer/server.js
+++ completeNode/webServer/server.js
@@ -7,11 +7,18 @@ app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello, Express!</h1>");
+  res.render("home.hbs", {
+    pageTitle: "Home Page",
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: "Welcome to my amazing website!"
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.send("About page");
+  res.render("about.hbs", {
+    pageTitle: "About Page",
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get("/bad", (req, res) => {
