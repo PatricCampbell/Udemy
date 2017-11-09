f3336a05a586970ac53a1cd42fac97c33ccf2973
Patric Campbell <patric@patriccampbell.com>
2017-11-09 10:51:10 -0500

    Change assertions from manual ones to 'expect' assertions

--- completeNode/nodeTests/utils/utils.test.js
+++ completeNode/nodeTests/utils/utils.test.js
@@ -1,15 +1,24 @@
+const expect = require("expect");
 const utils = require("./utils");
 
 it("should add two numbers", () => {
   const res = utils.add(33, 11);
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+
+  expect(res)
+    .toBe(44)
+    .toBeA("number");
+  // if (res !== 44) {
+  //   throw new Error(`Expected 44, but got ${res}`);
+  // }
 });
 
 it("should square a number", () => {
   const res = utils.square(5);
-  if (res !== 25) {
-    throw new Error(`Expected 25, but got ${res}`);
-  }
+
+  expect(res)
+    .toBe(25)
+    .toBeA("number");
+  // if (res !== 25) {
+  //   throw new Error(`Expected 25, but got ${res}`);
+  // }
 });
