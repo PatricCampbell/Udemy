4dcedd9c6ce496a877726974bd321a72e1bc7610
Patric Campbell <patric@patriccampbell.com>
2017-11-09 10:00:59 -0500

    Add middleware to log all requests to the server

--- completeNode/webServer/server.js
+++ completeNode/webServer/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 const app = express();
 
@@ -7,12 +8,27 @@ hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const method = req.method;
+  const url = req.url;
+  const log = `${now}: ${method} ${url}`;
+
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("Unable to append to server.log");
+    }
+  });
+  console.log(log);
+  next();
+});
+
 hbs.registerHelper("getCurrentYear", () => {
   return new Date().getFullYear();
 });
 
 hbs.registerHelper("screamIt", text => {
-  text.toUpperCase();
+  return text.toUpperCase();
 });
 
 app.get("/", (req, res) => {
