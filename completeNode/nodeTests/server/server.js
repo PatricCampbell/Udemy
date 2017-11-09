e747bf9bcd9c316c397bc282ea20e308ca793ece
Patric Campbell <patric@patriccampbell.com>
2017-11-09 11:33:52 -0500

    Add a root route and a test to test it

--- completeNode/nodeTests/server/server.js
+++ completeNode/nodeTests/server/server.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.status(404).send({ error: "error!", name: "Todo App v1.0" });
 });
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
+
+module.exports.app = app;
