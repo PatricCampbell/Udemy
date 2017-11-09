dbe1bbb994678f5996126d803fabc211fbcbf048
Patric Campbell <patric@patriccampbell.com>
2017-11-09 11:39:08 -0500

    Add a users route and test that it sends my user object

--- completeNode/nodeTests/server/server.js
+++ completeNode/nodeTests/server/server.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
   res.status(404).send({ error: "error!", name: "Todo App v1.0" });
 });
 
+// GET route /users returns an array of users
+// give users a name and age property, 2 or 3
+app.get("/users", (req, res) => {
+  res.send([{ name: "Patric", age: 30 }, { name: "Kristal", age: 24 }]);
+});
+
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
--- completeNode/nodeTests/server/server.test.js
+++ completeNode/nodeTests/server/server.test.js
@@ -14,3 +14,16 @@ it("should return hello world response", done => {
     })
     .end(done);
 });
+
+// make a new test
+// assert that status code is 200
+// assert that inside array, my user object exists with toInclude
+it("should include my user object", done => {
+  request(app)
+    .get("/users")
+    .expect(200)
+    .expect(res => {
+      expect(res.body).toInclude({ name: "Patric", age: 30 });
+    })
+    .end(done);
+});
