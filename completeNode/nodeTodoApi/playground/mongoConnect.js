f9ab620028e3d9ca78babacd916de07ae14471b4
Patric Campbell <patric@patriccampbell.com>
2017-11-09 15:44:49 -0500

    Finish playing around with mongodb

--- completeNode/nodeTodoApi/playground/mongoConnect.js
+++ completeNode/nodeTodoApi/playground/mongoConnect.js
@@ -1,4 +1,4 @@
-const MongoClient = require("mongodb").MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
 
 MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
   if (err) {
@@ -19,17 +19,17 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
 
   // insert new doc into Users collection (name, age, location)
 
-  db.collection("Users").insertOne({
-    name: "Patric",
-    age: 30,
-    location: "Brooooooooklyn"
-  },
-  (err, res) => {
-    if (err) {
-      return console.log("Unable to insert user", err);
-    }
-    console.log(JSON.stringify(res.ops, undefined, 2));
-  });
+  // db.collection("Users").insertOne({
+  //   name: "Patric",
+  //   age: 30,
+  //   location: "Brooooooooklyn"
+  // },
+  // (err, res) => {
+  //   if (err) {
+  //     return console.log("Unable to insert user", err);
+  //   }
+  //   console.log(JSON.stringify(res.ops[0]._id.getTimestamp(), undefined, 2));
+  // });
 
   db.close();
 });
