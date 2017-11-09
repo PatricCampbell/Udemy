6534323e62edaeeafddc53202636f71fccf47caa
Patric Campbell <patric@patriccampbell.com>
2017-11-09 10:41:28 -0500

    Add another test for a squaring function

--- completeNode/nodeTests/utils/utils.test.js
+++ completeNode/nodeTests/utils/utils.test.js
@@ -6,3 +6,10 @@ it("should add two numbers", () => {
     throw new Error(`Expected 44, but got ${res}`);
   }
 });
+
+it("should square a number", () => {
+  const res = utils.square(5);
+  if (res !== 25) {
+    throw new Error(`Expected 25, but got ${res}`);
+  }
+});
