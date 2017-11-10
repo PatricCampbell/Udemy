bac614b744b9c075d1fcda3bf18fb698d365b55c
Patric Campbell <patric@patriccampbell.com>
2017-11-10 15:20:38 -0500

    Add a test for the GET todos route

--- completeNode/nodeTodoApi/server/tests/server.test.js
+++ completeNode/nodeTodoApi/server/tests/server.test.js
@@ -4,8 +4,21 @@ const expect = require("expect");
 const { app } = require("../server");
 const { Todo } = require("../models/todo");
 
+const todos = [
+  {
+    text: "1st test todo"
+  },
+  {
+    text: "2nd test todo"
+  }
+];
+
 beforeEach(done => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({})
+    .then(() => {
+      return Todo.insertMany(todos);
+    })
+    .then(() => done());
 });
 
 describe("POST /todos", () => {
@@ -27,8 +40,8 @@ describe("POST /todos", () => {
         }
         Todo.find()
           .then(todos => {
-            expect(todos.length).toBe(1);
-            expect(todos[0].text).toBe(text);
+            expect(todos.length).toBe(3);
+            expect(todos[2].text).toBe(text);
             done();
           })
           .catch(err => done(err));
@@ -46,10 +59,22 @@ describe("POST /todos", () => {
         }
         Todo.find()
           .then(todos => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           })
           .catch(err => done(err));
       });
+  });
+});
+
+describe("GET /todos", () => {
+  it("should get all todos", done => {
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done());
   });
 });
