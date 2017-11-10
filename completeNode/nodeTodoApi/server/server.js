28957f331e27035c91e517a521404022d032cb50
Patric Campbell <patric@patriccampbell.com>
2017-11-10 13:53:03 -0500

    Refactor models into their own files

--- completeNode/nodeTodoApi/server/server.js
+++ completeNode/nodeTodoApi/server/server.js
@@ -1,30 +1,3 @@
-const mongoose = require("mongoose");
-
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
-
-const Todo = mongoose.model("Todo", {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
-});
-
-const User = mongoose.model("User", {
-  email: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  }
-});
+const mongoose = require("./db/mongoose");
+const Todo = require("./models/todo");
+const User = require("./models/user");
