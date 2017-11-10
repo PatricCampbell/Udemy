f5c18fdde8684f0b84c94641043502cf0c9eb589
Patric Campbell <patric@patriccampbell.com>
2017-11-10 13:42:35 -0500

    Add validations to todo model and add a user model

--- completeNode/nodeTodoApi/server/server.js
+++ completeNode/nodeTodoApi/server/server.js
@@ -5,12 +5,26 @@ mongoose.connect("mongodb://localhost:27017/TodoApp");
 
 const Todo = mongoose.model("Todo", {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
+  }
+});
+
+const User = mongoose.model("User", {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   }
 });
