d36496f684666388db0a0d4380c2b1e36ef368d9
Patric Campbell <patric@patriccampbell.com>
2017-11-10 14:10:08 -0500

    Add a todo post route to make new todo

--- completeNode/nodeTodoApi/server/server.js
+++ completeNode/nodeTodoApi/server/server.js
@@ -1,3 +1,27 @@
-const mongoose = require("./db/mongoose");
-const Todo = require("./models/todo");
-const User = require("./models/user");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { mongoose } = require("./db/mongoose");
+const { User } = require("./models/user");
+const { Todo } = require("./models/todo");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post("/todos", (req, res) => {
+  const todo = new Todo(req.body);
+
+  todo
+    .save()
+    .then(user => {
+      res.send(user);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
+});
+
+app.listen(3000, () => {
+  console.log("Server has started");
+});
