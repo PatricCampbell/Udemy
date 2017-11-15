0d1c72b094862c802a144198034f2d180c4c77ff
Patric Campbell <patric@patriccampbell.com>
2017-11-15 17:05:18 -0500

    Add a route to find one todo by id

--- completeNode/nodeTodoApi/server/server.js
+++ completeNode/nodeTodoApi/server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 const { mongoose } = require("./db/mongoose");
 const { User } = require("./models/user");
@@ -29,6 +30,26 @@ app.post("/todos", (req, res) => {
     })
     .catch(err => {
       res.status(400).send(err);
+    });
+});
+
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  const todo = Todo.findById(id)
+    .then(todo => {
+      if (todo) {
+        res.send(todo);
+      } else {
+        res.status(404).send();
+      }
+    })
+    .catch(err => {
+      res.status(400).send({});
     });
 });
 
