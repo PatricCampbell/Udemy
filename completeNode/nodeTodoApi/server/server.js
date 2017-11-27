19954d599155700c5b71c8544df17c55f2e52892
Patric <patric@patriccampbell.com>
2017-11-27 11:08:51 -0500

    Make app ready for heroku deployment

--- completeNode/nodeTodoApi/server/server.js
+++ completeNode/nodeTodoApi/server/server.js
@@ -7,6 +7,7 @@ const { User } = require("./models/user");
 const { Todo } = require("./models/todo");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -53,8 +54,8 @@ app.get("/todos/:id", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-  console.log("Server has started");
+app.listen(port, () => {
+  console.log(`Started server on port ${port}`);
 });
 
 module.exports = { app };
